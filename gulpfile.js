785cba5114394299a2987fa13b0491e3e28b9a5b
wad3inthewater <wfulle20@gmail.com>
2015-10-28 20:31:21 -0400

    setting up the build process for sass and jshinting for scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,31 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    jshint = require('gulp-jshint');
 
-gulp.task('default', function() {
-  console.log('gulped');
-  // place code for your default task here
+
+gulp.task('jshint', function() {
+  gulp.src('./public/javascript/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('sass', function(){
+  gulp.src('./public/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/csss'));
+});
+
+// gulp.task('sass:watch', function () {
+//   gulp.watch('./sass/**/*.scss', ['sass']);
+// });
+
+gulp.task('default',['jshint','sass'],  function() {
+  //watches for sass changes
+  gulp.watch('./public/sass/**/*.scss', function() {
+    gulp.run('sass');
+  });
+  //watches for js changes
+  gulp.watch('./public/javascript/**/*.js', function() {
+    gulp.run('jshint');
+  });
 });
---
+++ public/javascript/badscript.js
@@ -0,0 +1,7 @@
+var me = "Wade",
+    items = 4;
+
+function mystuff() {
+  me + item, = 10;
+}
+mystuff();
